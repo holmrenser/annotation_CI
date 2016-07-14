0e7178020d443c1832c51c410af89f891fb77545
Rens Holmer <rens.holmer@wur.nl>
2016-07-14 15:18:02 +0200

    test files

--- annotation_test.py
+++ annotation_test.py
@@ -2,15 +2,31 @@
 """
 """
 
+import sys
 import gff_toolkit as gt
 
-def main():
-	fasta_file = 'test.fasta'
-	gff_file = 'test.gff'
-	gff = gt.parser(gff_file,fasta_file=fasta_file)
-	for transcript in gff.getitems(featuretype='mRNA'):
-		assert transcript.pep[0] == 'M'
-		assert transcript.pep[-1] == '*'
+fasta_file = 'test.fasta'
+gff_success = 'test_success.gff'
+gff_fail = 'test_fail.gff'
+
+def test1():
+	for gff_file in (gff_success,gff_fail):
+		gff = gt.parser(gff_file,fasta_file=fasta_file)
+		for transcript in gff.getitems(featuretype='mRNA'):
+			if transcript.pep[0] != 'M':
+				print gff_file
+				print transcript.pep
+				print transcript
+				raise Exception('Wrong start')
+			if transcript.pep[-1] != '*':
+				print gff_file
+				print transcript.pep
+				print transcript
+				raise Exception('Wrong stop')
+
+def test2():
+	assert True
 
 if __name__ == '__main__':
-	main(*sys.argv[1:])
+	test1()
+	test2()
