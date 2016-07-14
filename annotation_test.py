36634236c8d50f5f3288c0b3f301d90f39fdba0a
Rens Holmer <rens.holmer@wur.nl>
2016-07-14 14:23:22 +0200

    test files

--- annotation_test.py
+++ annotation_test.py
@@ -5,7 +5,12 @@
 import gff_toolkit as gt
 
 def main():
-	assert True
+	fasta_file = 'test.fasta'
+	gff_file = 'test.gff'
+	gff = gt.parser(gff_file,fasta_file=fasta_file)
+	for transcript in gff.getitems(featuretype='mRNA'):
+		assert transcript.pep[0] == 'M'
+		assert transcript.pep[-1] == '*'
 
 if __name__ == '__main__':
 	main(*sys.argv[1:])
